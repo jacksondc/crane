10b483c2def4c703b31e4d286946251a8e841024
Jackson de Campos <jacksondecampos@gmail.com>
2015-08-14 15:38:39 -0700

    Make js player a little more interesting

--- examples/rps/players/js-player.js
+++ examples/rps/players/js-player.js
@@ -1,9 +1,9 @@
-//initialize things here if necessary
+//initialize things globally if necessary
 var move = "rock";
 
 function respond(command) {
   if(command === "move")
-    return move;
+    return Math.random() < 0.5 ? "rock" : "paper";
   else
     return "err";
 }
